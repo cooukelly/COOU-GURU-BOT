b4d33f91047351da4619eb5b7027873922a8a729
cooukelly <147151001+cooukelly@users.noreply.github.com>
2023-10-06 13:56:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['08057536207', 'me', true],
+  ['08057536207', 'COOU', true], 
   [''] 
 ] //Number of owners
 
